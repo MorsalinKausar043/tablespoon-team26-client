ec1c2c03f26fbf87679a024e582792fe8fffe013
Morsalin <morsalinkausar043@gmail.com>
2022-01-02 17:06:10 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home.Js';
 
 function App() {
   return (
     <div className="App">
       <h1>TableSpoon - doing roar Journey</h1>
       <h2>its BackENd job</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur adipisci sed officia nulla voluptatibus quasi corporis illum excepturi, quas maiores? Ipsam illo blanditiis, iste officia praesentium ipsa dolorem est impedit.</p>
     </div>
   );
 }
