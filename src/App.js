277dc70f872d382a9a95dd879208cf903056aa8e
Morsalin <morsalinkausar043@gmail.com>
2022-01-02 17:10:39 +0600

    morsalin done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>TableSpoon - doing roar Journey</h1>
-      <h2>its BackENd job</h2>
+      <h2>its BackENd job..!</h2>
     </div>
   );
 }
